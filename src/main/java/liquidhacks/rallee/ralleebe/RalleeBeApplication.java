2191af967147c1a61deca5a4d04bb067b792e315
Emily Clark <eclark8395@gmail.com>
2020-11-06 19:54:38 -0600

    spring initializr

---
+++ src/main/java/liquidhacks/rallee/ralleebe/RalleeBeApplication.java
@@ -0,0 +1,13 @@
+package liquidhacks.rallee.ralleebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RalleeBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RalleeBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/liquidhacks/rallee/ralleebe/RalleeBeApplicationTests.java
@@ -0,0 +1,13 @@
+package liquidhacks.rallee.ralleebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RalleeBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
