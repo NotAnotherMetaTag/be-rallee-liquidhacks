1bdd937d76191b80275caf6e2a7f194b76c2967c
Emily Clark <eclark8395@gmail.com>
2020-11-07 02:42:33 -0600

    functional state for get and post

--- src/main/java/liquidhacks/rallee/ralleebe/controller/rest/Register.java
+++ src/main/java/liquidhacks/rallee/ralleebe/controller/rest/Register.java
@@ -1,6 +1,8 @@
 package liquidhacks.rallee.ralleebe.controller.rest;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,13 +21,23 @@ class Register {
 	}
 
 	@PostMapping("/register")
-	public void register(@RequestBody UserDTO user){
-		user.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
+	public void register(String username, String password){
+		UserDTO user = new UserDTO();
+		user.setPassword(new BCryptPasswordEncoder().encode(password));
+		user.setId(UUID.randomUUID());
+		user.setUsername(username);
 		userRepo.save(user);
 	}
 	
-	@GetMapping("test")
-	public Iterable getRegisteredUsers() {
+	@GetMapping("/test")
+	public Optional<UserDTO> getRegisteredByUsername(String id) {
+
+		return userRepo.findById(id);
+	}
+	
+	
+	@GetMapping("/getAll")
+	public List<UserDTO> getAllRegisteredUsers() {
 		return userRepo.findAll();
 	}
 }
