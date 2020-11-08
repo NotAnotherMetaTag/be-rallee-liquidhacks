2a81a6a9259d47f6918d2424efeced27b3364888
Alex Leska <deskmicealex@gmail.com>
2020-11-08 02:42:12 -0600

    Docker-compose added

---
+++ localdb_migration.sh
@@ -0,0 +1,21 @@
+aws dynamodb create-table \
+ --table-name User \
+ --attribute-definitions \
+     AttributeName=id,AttributeType=S \
+ --key-schema \
+     AttributeName=id,KeyType=HASH \
+ --provisioned-throughput \
+     ReadCapacityUnits=5,WriteCapacityUnits=5 \
+ --region us-west-1 \
+ --endpoint-url http://localhost:8000
+
+aws dynamodb create-table \
+ --table-name Player \
+ --attribute-definitions \
+    AttributeName=email,AttributeType=S \
+ --key-schema \
+    AttributeName=email,KeyType=HASH \
+ --provisioned-throughput \
+    ReadCapacityUnits=5,WriteCapacityUnits=5 \
+ --region us-west-1 \
+ --endpoint-url http://localhost:8000
